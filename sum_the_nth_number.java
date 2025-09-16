ac4eb952cc2148cbf8a80f0a175947160105a9e2
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-09-16 23:27:13 +0530

    Create sum_the_nth_number.java

---
+++ sum_the_nth_number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	  Scanner sc = new Scanner(System.in);
+	  System.out.print("Enter the n number : ");
+	  int num = sc.nextInt();
+	 
+	  System.out.println((num*(num + 1)/2));
+	  
+	 
+	}
+}
