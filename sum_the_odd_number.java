c5b6440471853b1bb95dcf6d33380c9264472574
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-09-16 23:41:33 +0530

    Create sum_the_odd_number.java

---
+++ sum_the_odd_number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	  Scanner sc = new Scanner(System.in);
+	  System.out.print("Enter the n number : ");
+	  int num = sc.nextInt();
+	  
+	 if(num%2==0) 
+	  System.out.println((num/2)); //[1,2,3,4,5]
+	  else{
+	      System.out.println((num/2)+ 1);
+	  }
+	  
+	 
+	}
+}
