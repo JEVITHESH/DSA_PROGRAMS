7409b3f27b23695a699e620d4af8e9ceb5e94c6d
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-09-20 22:19:46 +0530

    Update fact_number.java

--- fact_number.java
+++ fact_number.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       System.out.print("Enter the Number : ");
-       int a = sc.nextInt();
-
-       System.out.println("Factors of " + a + " are:");
-       for(int i = 1; i * i <= a; i++){
-           if(a % i == 0){
-               System.out.println(i);       // first factor
-               if(i != a / i){              // avoid printing sqrt twice
-                   System.out.println(a / i); // pair factor root(n)
-               }
-           }
-       }
+       System.out.print("Enter the number : ");
+       int n= sc.nextInt();
+        for(int i = 1 ; i*i >= 0 ; i++){//find the A value(a*b=n)->b=n/i
+            if(n%i==0){
+                if(i*i != n){
+                    System.out.print(n/i+" ");
+                }  
+            }
+        }
     }
 }
