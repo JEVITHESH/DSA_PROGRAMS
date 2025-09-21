e9e78db30735fcb59a4d85b5f8f638c99ced9b02
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-09-21 11:51:20 +0530

    Update sum_the_nth_number.java

--- sum_the_nth_number.java
+++ sum_the_nth_number.java
@@ -6,8 +6,13 @@ public class Main
 	  System.out.print("Enter the n number : ");
 	  int num = sc.nextInt();
 	 
-	  System.out.println((num*(num + 1)/2));
+	  System.out.println((num*(num + 1)/2)); //(5*(5+1)/2)
+		                                     //5*(6)/2
+		                                     //30/2 => 15
 	  
 	 
 	}
 }
+//output
+//Enter the n number : 5
+//15
