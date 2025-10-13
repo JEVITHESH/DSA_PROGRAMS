1d200b7138f58c3d3ee675eea3106d435df25f42
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-10-13 11:45:17 +0530

    Create InsertAtEnd.java

---
+++ LinkedList/InsertAtEnd.java
@@ -0,0 +1,69 @@
+
+class node{
+    int data;
+    node next;
+    node(int _data){
+        data = _data;
+        next = null;
+        //System.out.print(data+" -> ");
+    }
+}
+class LinkedList{
+    node head = null;
+    node tail = null;
+    public void addnode(int _data){
+        node newnode = new node(_data);
+        if(head == null){
+            head = newnode;
+            tail = newnode;
+        }
+        else{
+            tail.next = newnode;
+            tail = newnode;
+        }
+    }
+    public void insertatfirst(int _data){
+        node newnode = new node(_data);
+        if(head == null){
+            head = newnode;
+            tail = newnode;
+        }
+        else{
+            newnode.next = head;
+            head = newnode;
+        }
+        
+            }
+    public void insertatend(int _data){
+        node newnode = new node(_data);
+        if(head == null){
+            head = newnode;
+            tail = newnode;
+        }
+        else{
+            tail.next = newnode;
+            tail = newnode;
+        }
+        
+            }
+            public void printlist(){
+            node temp = head;
+            while(temp != null){
+                System.out.print(temp.data +" -> ");
+                temp = temp.next;
+        }
+    }
+    
+}
+public class Main
+{
+	public static void main(String[] args) {
+       LinkedList num = new LinkedList();
+       num.addnode(5);
+       num.addnode(10);
+       num.insertatfirst(2);
+       num.insertatend(15);
+       num.printlist();
+	}
+}
+output : 2 -> 5 -> 10 -> 15 ->
