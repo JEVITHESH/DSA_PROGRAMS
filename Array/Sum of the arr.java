45625165b7ff436a21e943ec0c6629349e4404bc
JEVITHESH S <151340536+JEVITHESH@users.noreply.github.com>
2026-05-21 10:02:45 +0530

    Implement sum calculation for an array

---
+++ Array/Sum of the arr.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    // Method to calculate sum
+    public static int findSum(int[] arr) {
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Calling method
+        int result = findSum(arr);
+
+        System.out.println(result);
+    }
+}
