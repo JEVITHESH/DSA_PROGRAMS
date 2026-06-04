668b0715e1042db4f57d75d0221200dd805db12b
JEVITHESH S <151340536+JEVITHESH@users.noreply.github.com>
2026-06-04 13:09:54 +0530

    Create w pattern.java

---
+++ placement/w pattern.java
@@ -0,0 +1,25 @@
+public class Main
+{ 
+    
+	public static void main(String[] args) {
+	    int n = 5;
+	    for(int row = 1 ; row <= n ; row++){
+	        for(int spa = 1 ; spa <= row-1 ; spa++){
+	            System.out.print(" ");
+	           
+	        }
+	        for(int str = 1 ; str <= (n*2)-(row+(row-1)) ; str++){
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	        }	
+	}
+	        
+	    }
+
+
+*********
+ *******
+  *****
+   ***
+    *
