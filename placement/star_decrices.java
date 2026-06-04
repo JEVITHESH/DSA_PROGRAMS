a65488fbf839f835539ec27e8564e0ff677fb576
JEVITHESH S <151340536+JEVITHESH@users.noreply.github.com>
2026-06-04 12:14:20 +0530

    Change star pattern to number pattern in Main class
    Modified the star pattern printing logic to print numbers instead of stars.

--- placement/star_decrices.java
+++ placement/star_decrices.java
@@ -19,3 +19,47 @@ public class Main
 ***
 **
 *
+
+--------------------------------------------------------------------------------------------------------------------------	
+public class Main
+{ 
+    
+	public static void main(String[] args) {
+	    int n = 5;
+	    for(int row = 1 ; row <= n ; row++){
+	        for(int col = 1 ; col <= n-(row-1) ; col++){
+	            System.out.print(n-(row-1));
+	           
+	        }
+	        System.out.println();
+	        }	
+	}
+	        
+	    }
+55555
+4444
+333
+22
+1
+-------------------------------------------------------------------------------------------------------------------------
+public class Main
+{ 
+    
+	public static void main(String[] args) {
+	    int n = 5;
+	    for(int row = 1 ; row <= n ; row++){
+	        for(int col = 1 ; col <= n-(row-1) ; col++){
+	            System.out.print(col);
+	           
+	        }
+	        System.out.println();
+	        }	
+	}
+	        
+	    }
+12345
+1234
+123
+12
+1
+-------------------------------------------------------------------------------------------------------------------------
