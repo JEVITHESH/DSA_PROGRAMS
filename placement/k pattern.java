e88b7cf746370c8315143297b3ed6f2a02b583c8
JEVITHESH S <151340536+JEVITHESH@users.noreply.github.com>
2026-06-04 12:23:45 +0530

    Create k pattern.java

---
+++ placement/k pattern.java
@@ -0,0 +1,24 @@
+public class Main
+{ 
+    
+	public static void main(String[] args) {
+	    int n = 5;
+	    for(int row = 1 ; row <= n ; row++){
+	        for(int spa = 1 ; spa <= n-row ; spa++){
+	            System.out.print(" ");
+	           
+	        }
+	        for(int str = 1 ; str <= row ; str++){
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	        }	
+	}
+	        
+	    }
+
+    *
+   **
+  ***
+ ****
+*****
