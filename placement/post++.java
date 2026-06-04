01ce23224095ebd40393fdd7afa5b01c2ab9acf7
JEVITHESH S <151340536+JEVITHESH@users.noreply.github.com>
2026-06-04 11:34:29 +0530

    Add Main class to print number pattern

---
+++ placement/post++.java
@@ -0,0 +1,23 @@
+public class Main
+{ 
+    
+	public static void main(String[] args) {
+	    int n = 15;
+	    int aa = 1;
+	    for(int i = 1 ; i <= n ; i++){
+	        for(int j = 1 ; j <= i ; j++){
+	            System.out.print( aa +" ");
+	              aa++;
+	        }	System.out.println();
+	        
+	        
+	    }
+	
+	}
+}
+
+1
+23
+456
+78910
+111213
