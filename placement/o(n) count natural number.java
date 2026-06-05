c2417d0a0b4e11a0ef7c1234e0928cf67af55bd3
JEVITHESH S <151340536+JEVITHESH@users.noreply.github.com>
2026-06-05 19:48:19 +0530

    Create o(n) count natural number.java

---
+++ placement/o(n) count natural number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Type the total number : ");
+	    int n = sc.nextInt();
+	    
+	    System.out.println((n*(n+1))/2);
+	   
+	}
+}
