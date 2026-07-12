3d93888af6c9373d5b4fe7586de7063ff1b0ea08
JEVITHESH S <jev0760@gmail.com>
2026-07-13 00:13:47 +0530

    Create right rotate array in k times.java

---
+++ Array/right rotate array in k times.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	 int [] arr = new int[5];
+	 Scanner sc = new Scanner(System.in);
+	 for(int i = 0 ; i < arr.length ; i++){
+	    arr[i] = sc.nextInt(); 
+	 }
+	 int k = 3;
+	 for(int j = 0 ; j<k ; j++ ){
+	 int tem = arr[arr.length - 1];
+	 for(int i=arr.length-1 ;i>0; i--){
+	     arr[i]=arr[i-1];
+	 }
+	 arr[0] = tem;
+	 }
+	 for(int arr1 : arr){
+	     System.out.print(arr1);
+	 }
+	
+	}
+}
